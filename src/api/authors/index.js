3fde5fc9c4752272baa10d185c2ea2faf265cc43
McNoHam14 <McNoHam14@gmail.com>
2023-03-06 13:23:47 +0000

    error handlers added

--- src/api/authors/index.js
+++ src/api/authors/index.js
@@ -1,10 +1,11 @@
 // CRUD endpoints
 
 import Express from "express";
-import fs from "fs";
+import fs, { writeFile } from "fs";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import uniqid from "uniqid";
+import createHttpError from "http-errors";
 
 const authorsRouter = Express.Router();
 
@@ -24,90 +25,123 @@ const writeAuthors = (authorsArray) =>
 
 // POST (new author)
 
-authorsRouter.post("/", (req, res) => {
-  // res.send({ message: "Hello I am the POST endpoint" });
-  console.log("A", req.body);
-  const newAuthor = {
-    ...req.body,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    id: uniqid(),
-  };
+authorsRouter.post("/", (req, res, next) => {
+  try {
+    const newAuthor = {
+      ...req.body,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: uniqid(),
+    };
 
-  const authorsArray = getAuthors();
+    const authorsArray = getAuthors();
 
-  authorsArray.push(newAuthor);
+    const emailInUse = authorsArray.some(
+      (author) => author.email === req.body.email
+    );
 
-  console.log("B", newAuthor);
-
-  writeAuthors(authorsArray);
-
-  res.status(201).send({ id: newAuthor.id });
+    if (!emailInUse) {
+      authorsArray.push(newAuthor);
+      writeAuthors(authorsArray);
+      res.status(201).send({
+        name: newAuthor.name + " " + newAuthor.surname,
+        id: newAuthor.id,
+      });
+    } else {
+      next(
+        createHttpError(
+          400,
+          `${req.body.email} is already registered to another user `
+        )
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // GET (all authors)
 
-authorsRouter.get("/", (req, res) => {
-  const fileContentAsBuffer = fs.readFileSync(authorsJSONPath);
-  console.log("C", fileContentAsBuffer);
-
-  console.log("D", JSON.parse(fileContentAsBuffer));
-
-  const authorsArray = getAuthors();
-
-  res.send(authorsArray);
+authorsRouter.get("/", (req, res, next) => {
+  try {
+    const authorsArray = getAuthors();
+    res.send(authorsArray);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // GET (single author)
 
-authorsRouter.get("/:authorId", (req, res) => {
-  //   res.send({ message: "Single author endpoint " });
-
-  //   console.log("ID:", req.params.userId);
-
-  const authorsArray = getAuthors();
-
-  const author = authorsArray.find(
-    (author) => author.id === req.params.authorId
-  );
-
-  console.log(author);
-
-  res.send(author);
+authorsRouter.get("/:authorId", (req, res, next) => {
+  try {
+    const authorsArray = getAuthors();
+    const foundAuthor = authorsArray.find(
+      (author) => author.id === req.params.authorId
+    );
+    if (foundAuthor) {
+      res.send(foundAuthor);
+    } else {
+      next(
+        createHttpError(404, `Author with id ${req.params.authorId} not found`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // PUT
 
-authorsRouter.put("/:authorId", (req, res) => {
-  const authorsArray = getAuthors();
-
-  const index = authorsArray.findIndex(
-    (author) => author.id === req.params.authorId
-  );
-
-  const oldAuthor = authorsArray[index];
-
-  const updatedAuthor = { ...oldAuthor, ...req.body, updatedAt: new Date() };
-
-  authorsArray[index] = updatedAuthor;
-
-  writeAuthors(authorsArray);
-
-  res.send(updatedAuthor);
+authorsRouter.put("/:authorId", (req, res, next) => {
+  try {
+    const authorsArray = getAuthors();
+    const index = authorsArray.findIndex(
+      (author) => author.id === req.params.authorId
+    );
+    if (index !== -1) {
+      const oldAuthor = authorsArray[index];
+      const updatedAuthor = {
+        ...oldAuthor,
+        ...req.body,
+        updatedAt: new Date(),
+      };
+      authorsArray[index] = updatedAuthor;
+      writeAuthors(authorsArray);
+      res.send(updatedAuthor);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `There is no author with this ${req.body.authorId} id`
+        )
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // DELETE
 
-authorsRouter.delete("/:authorId", (req, res) => {
-  const authorsArray = getAuthors();
+authorsRouter.delete("/:authorId", (req, res, next) => {
+  try {
+    const authorsArray = getAuthors();
+    const remainingAuthors = authorsArray.filter(
+      (author) => author.id !== req.params.authorId
+    );
 
-  const remainingAuthors = authorsArray.filter(
-    (author) => author.id !== req.params.authorId
-  );
-
-  writeAuthors(authorsArray);
-
-  res.status(204).send();
+    if (authorsArray.length !== remainingAuthors.length) {
+      writeAuthors(remainingAuthors);
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(404, `Author with ${req.params.authorId} id not found`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // POST (checkEmail)
